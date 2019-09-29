2c3ef01da7bcb25f52a8a1f56256318ac0edc878
Kate <kalabro@ya.ru>
2019-09-29 14:16:36 +0300

    Hello World: Added slight animation.

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,18 +1,49 @@
-import React, {useState} from 'react';
-import {Text, TextInput, SafeAreaView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  Animated,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    height: 60,
+    textAlign: 'center',
+  },
+});
 
 const HelloWorldApp = () => {
   const [userName, setUserName] = useState();
   const [formText, setFormText] = useState('');
+  const [animatedText] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    if (userName) {
+      Animated.timing(animatedText, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [animatedText, userName]);
 
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <SafeAreaView style={styles.containerView}>
       {userName ? (
-        <Text>Hello, {userName}!</Text>
+        <Animated.Text style={{opacity: animatedText}}>
+          Hello, {userName}!
+        </Animated.Text>
       ) : (
         <TextInput
-          style={{alignSelf: 'stretch', height: 60, textAlign: 'center'}}
+          style={styles.textInput}
           placeholder="Type your name"
           onChangeText={text => setFormText(text)}
           onSubmitEditing={() => setUserName(formText)}
